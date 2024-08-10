15772eba76f0456fce95f4eba708b6e9d742559c
Fernando Jorge Mota <contato@fjorgemota.com>
2024-08-10 14:25:32 -0300

    Improve /start command

--- main.go
+++ main.go
@@ -24,12 +24,14 @@ func main() {
 		// Handle /start command
 		bot.Handle("/start", func(c tele.Context) error {
 			message := strings.Replace("Welcome to YourBotUsername!\n\n"+
-				"This bot works only with inline queries. Here's how to use it:\n"+
+				"**YourBotUsername** is a Telegram inline bot that transforms Twitter, X (formerly Twitter), and Instagram URLs into optimized versions with enhanced media previews. This bot is particularly useful for quickly sharing media-rich content with large previews directly within Telegram chats.\n"+
+				" Here's how to use it:\n"+
 				"1. Open any chat (with this bot added) and type @YourBotUsername followed by a URL.\n"+
 				"2. For example, try entering:\n"+
 				"   - @YourBotUsername https://twitter.com/someuser/status/12345\n"+
-				"   - @YourBotUsername https://instagram.com/someuser\n\n"+
-				"The bot will automatically transform Twitter and Instagram URLs to their equivalent versions for preview.\n", "YourBotUsername", bot.Me.Username, -1)
+				"   - @YourBotUsername https://instagram.com/reels/1234\n\n"+
+				"The bot will automatically transform Twitter and Instagram URLs to their equivalent versions for preview.\n"+
+				"You can find the source code for this bot on GitHub: [EmbedPreviewBot Repository](https://github.com/fjorgemota/EmbedPreviewBot)", "YourBotUsername", bot.Me.Username, -1)
 
 			return c.Send(message)
 		})
